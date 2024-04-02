6c6326b74fc98e2635bd007bd3ff43bfe5f2165d
Luorong <lojung@pku.edu.cn>
2024-04-02 18:11:31 +0800

    'mask_ROI_select_uodate'

--- select_ROI.m
+++ select_ROI.m
@@ -1,4 +1,4 @@
-function [rois, traces] = select_ROI(movie, nrows, ncols, t, colors, mask, map)
+function [bwmask, traces] = select_ROI(movie, nrows, ncols, t, colors, mask, map)
 % 
 % ----------Write by Liu-Yang Luorong and ChatGPT----------
 % ----------POWERED by Zoulab in Peking University----------
@@ -45,9 +45,10 @@ function [rois, traces] = select_ROI(movie, nrows, ncols, t, colors, mask, map)
 
 
 % Initialize variables
-rois = {};
 traces = [];
+bwmask = zeros(nrows,ncols);
 movie_2D = reshape(movie, ncols, nrows, []);
+
 % Set current figure to full screen and add keypress callback
 fig = gcf;
 set(fig,'Position',get(0,'Screensize'));
@@ -59,26 +60,42 @@ if ~isempty(map)
     map_axe = gca;
     imagesc(map);
     colorbar;
-    title(sprintf(['Sensitivity MAP\n\nPress SPACE to continue\nPress ENTER to end' ...
-        '\n\n!!!!!!!!!!----------THE LAST ROI MUST BE BACKGROUND----------!!!!!!!!!!']));
+    title(sprintf('Sensitivity MAP\n\nPress SPACE to continue\nPress ENTER to end'));
     hold on;
     axis image;
+
+    % Display fluorescent image
+    subplot(2,2,2);
+    image_axe = gca;
+    im_adj = imadjust(uint16(mean(movie_2D, 3)));
+    imshow(im_adj);
+    hold on;
+    title('Fluorescent Image');
+
+    % Plot Trace
+    subplot(2,2,4);
+    trace_axe = gca;
+    hold on;
+    xlabel('Time (s)');
+    ylabel('Intensity');
+
+else
+% Display fluorescent image without map
+    subplot(1,2,1);
+    image_axe = gca;
+    im_adj = imadjust(uint16(mean(movie_2D, 3)));
+    imshow(im_adj);
+    hold on;
+    title('Fluorescent Image');
+
+    % Plot Trace
+    subplot(1,2,2);
+    trace_axe = gca;
+    hold on;
+    xlabel('Time (s)');
+    ylabel('Intensity');
 end
 
-% Display fluorescent image
-subplot(2,2,2);
-image_axe = gca;
-im_adj = imadjust(uint16(mean(movie_2D, 3)));
-imshow(im_adj);
-hold on;
-title('Fluorescent Image');
-
-% Plot Trace
-subplot(2,2,4);
-trace_axe = gca;
-hold on;
-xlabel('Time (s)');
-ylabel('Intensity');
 
 % Mask provided, overlay ROI and traces
 if ~isempty(mask)
@@ -96,19 +113,23 @@ if ~isempty(mask)
 else
     % ROI selection process
     while true
+        num_roi = max(bwmask(:)) + 1;
         if ~isempty(map)
-            roi_mask = drawpolygon('Color', colors(mod(length(rois), length(colors)) + 1, :), 'LineWidth', 1, 'Parent', map_axe);
+            roi_mask = drawpolygon('Color', colors(mod(num_roi, length(colors)) + 1, :), 'LineWidth', 1, 'Parent', map_axe);
         else
-            roi_mask = drawpolygon('Color', colors(mod(length(rois), length(colors)) + 1, :), 'LineWidth', 1, 'Parent', image_axe);
+            roi_mask = drawpolygon('Color', colors(mod(num_roi, length(colors)) + 1, :), 'LineWidth', 1, 'Parent', image_axe);
         end
         mask = poly2mask(roi_mask.Position(:, 1), roi_mask.Position(:, 2), size(im_adj, 1), size(im_adj, 2));
-        rois{end + 1} = mask;
+        bwmask(mask) = num_roi;
+
         trace = mean(movie(mask, :));
         traces = [traces trace'];
 
         boundary = bwboundaries(mask);
-        plot(boundary{1}(:, 2), boundary{1}(:, 1), 'Color', colors(mod(length(rois) - 1, length(colors)) + 1, :), 'LineWidth', 1, 'Parent', image_axe);
-        plot(t, trace, 'Color', colors(mod(length(rois) - 1, length(colors)) + 1, :), 'Parent', trace_axe);
+        if ~isempty(map)
+            plot(boundary{1}(:, 2), boundary{1}(:, 1), 'Color', colors(mod(num_roi - 1, length(colors)) + 1, :), 'LineWidth', 1, 'Parent', image_axe);
+        end
+        plot(t, trace, 'Color', colors(mod(num_roi - 1, length(colors)) + 1, :), 'Parent', trace_axe);
 
         % Wait for user input
         fig.UserData = [];
